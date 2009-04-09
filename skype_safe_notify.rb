575b855090a6d6a8d3ad7c13a6ee680d18fa901d
Kiss Péter <ypetya@gmail.com>
2009-04-09 10:15:03 +0200

    Some embed code recognize for foreign Pages modifications. Not working yet :-/

--- skype_safe_notify.rb
+++ skype_safe_notify.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
 #
 # Kiss Péter - ypetya@gmail.com
 #
 # Anti injection script
 
+#requirements
 require 'rubygems'
+require 'nokogiri'
 require 'mechanize'
 
 load '/etc/my_ruby_scripts/settings.rb'
@@ -16,53 +19,67 @@ BLOG_NAME = 'csakacsuda'
 message= ARGV.join(' ')
 
 File.open("#{DIR}/#{FILENAME}",'w') do |f|
-  f.puts message 
+  f.puts message
 end
 
 EMBED_CODES= {
 
-  :vimeo => {:get_id => /http:\/\/vimeo\.com\/(.*)$/, 
+  :vimeo => {:get_id => /http:\/\/vimeo\.com\/(.*)$/,
     :code =>'<object width="400" height="225"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=EMBEDCODE&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=EMBEDCODE&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="400" height="225"></embed></object>'},
-  :youtube => { :get_id => /http:\/\/www\.youtube\.com\/watch\?v=(.*)/, 
+  :youtube => { :get_id => /http:\/\/www\.youtube\.com\/watch\?v=(.*)/,
     :code => '<object width="425" height="344"><param name="movie" value="http://www.youtube.com/v/EMBEDCODE&hl=en&fs=1"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/EMBEDCODE&hl=en&fs=1" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="425" height="344"></embed></object>'},
 }
 
-def simple_format link
-  link_as_html = %{<a href="#{link}">#{link}</a>} 
-  EMBED_CODES.each do |k,v|
-    link.gsub(v[:get_id]) do
-      my_id = $1.dup
-      link = v[:code].dup.gsub(/EMBEDCODE/){  my_id }
-      link += '<br/>' + link_as_html
-      return link
+def recognize_first_embed_video_code_at link
+  agent, agent.user_agent_alias, agent.redirect_ok = WWW::Mechanize.new, 'Linux Mozilla', true
+  oldal = agent.get(link)
+  if oldal.is_a? WWW::Mechanize::Page
+    if o = oldal/"embed"
+      return o.first.to_xhtml unless o.blank?
     end
   end
-  link_as_html
+  nil
+end
+
+def simple_format link
+  link_as_html = %{<a href="#{link}">#{link}</a>}
+
+  if embed_code = recognize_first_embed_video_code_at( link )
+    embed_code += '<br/>'
+  else
+    EMBED_CODES.each do |k,v|
+      link.gsub(v[:get_id]) do
+        my_id = $1.dup
+        link = v[:code].dup.gsub(/EMBEDCODE/){  my_id }
+        link += '<br/>' + link_as_html
+        return link
+      end
+    end
+  end
+  return (embed_code ? embed_code : '') + link_as_html
 end
 
 def push_to_freeblog email,password,message
-  #return if message['text'] =~ /@|http/
   agent, agent.user_agent_alias, agent.redirect_ok = WWW::Mechanize.new, 'Linux Mozilla', true
   f = agent.get('http://freeblog.hu').forms.select {|lf| lf.action == 'fblogin.php'}.first
   @@freeblogpwd ||= password
-  f.username, f.password = email,password 
+  f.username, f.password = email,password
   f.checkboxes.first.uncheck
   m = agent.submit(f)
-  #m = agent.get("http://admin.freeblog.hu/edit/#{@@freebloguser}/entries")
   m = agent.get("http://admin.freeblog.hu/edit/#{BLOG_NAME}/entries/create-entry")
   m.forms.first.fields.select { |f| f.name =~ /CONTENT/ }.first.value = message
 
   agent.submit(m.forms.first)
 
   puts 'freeblog -> OK'
-#rescue
-#  puts 'freeblog -> ERROR' 
 end
 
 # check for new links
 
 new_links = []
 new_links_html = []
+# recognized foreign embed video codes
+foreign_video_embed_codes = []
 
 message.gsub(Regexp.new(URI.regexp.source.sub(/^[^:]+:/, '(http|https):'), Regexp::EXTENDED, 'n')) do
   new_links << $&
@@ -78,7 +95,7 @@ new_links.each do |link|
   agent, agent.user_agent_alias, agent.redirect_ok = WWW::Mechanize.new, 'Linux Mozilla', true
   oldal = agent.get( "http://#{BLOG_NAME}.freeblog.hu" )
   unless oldal.links.map{|l| l.href}.include? link
-    if NOT_VALID_URL.map{|r| link =~ r}.select{|x|x}.empty?   
+    if NOT_VALID_URL.map{|r| link =~ r}.select{|x|x}.empty?
       new_links_html << link
     end
   end
