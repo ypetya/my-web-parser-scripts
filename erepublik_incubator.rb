8732435348867f3f6ae079531da328944c37c791
Kiss Péter <ypetya@gmail.com>
2009-09-03 12:17:02 +0200

    fx

--- erepublik_incubator.rb
+++ erepublik_incubator.rb
@@ -11,33 +11,45 @@ DIR = "/home/#{ENV['USER']}"
 @@site = "http://www.erepublik.com"
 
 def login 
-  f = @@agent.get( @@site ).forms.first
-  f.citizen_name, f.citizen_password = @@settings[:erep].first, @@settings[:erep].last
-  yield( f.submit )
+  l = @@agent.get( @@site )
+
+  if f = l.forms.first
+    begin
+      if f.citizen_name
+        f.citizen_name, f.citizen_password = @@settings[:erep].first, @@settings[:erep].last
+        return yield( f.submit )
+      end
+    rescue
+    end
+  end
+  yield(l)
 end
 
 def select_link text, page
-  o = @@agent.get("#{@@site}#{page}").links.select{ |l| l.text == text.capitalize }
-  o.size > 0 ? o.first : 'ERR'
+  o = page.links.select{ |l| l.text == text.capitalize }
+  yield(o.size > 0 ? @@agent.get("#{@@site}#{o.first.href}") : 'ERR')
 end
 
 def work_with login
-  select_link( 'Company', login  ) do company
-    select_link( 'Work', company ) do work
+  select_link( 'Company', login  ) do |company|
+    sleep 2
+    select_link( 'Work', company ) do |work|
       puts 'worked..' unless work == 'ERR'
     end
   end
 end
 
 def train_with login
-  select_link( 'Army', login ) do army
-    select_link( 'Train', army) do train
+  select_link( 'Army', login ) do |army|
+    sleep 2
+    select_link( 'Train', army) do |train|
       puts 'trained..' unless train == 'ERR'
     end
   end
 end
 
 login do |l|
+  sleep 2 
   work_with l
   train_with l
 end
